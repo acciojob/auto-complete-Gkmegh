92aae04474593299a848d41ddbf3045690356b64
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2024-02-25 13:29:08 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,42 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [input, setInput]= useState("")
+  const [fruitsList, setFruitsList] = useState(fruits)
+
+  function HandleChange(e){
+    setInput(e.target.value)
+  }
+
+  function setList(){
+    if(input!==""){
+    setFruitsList(fruitsList.filter(fruit => fruit.toLowerCase().includes(input.toLowerCase().trim())))
+    }
+    else{
+        setFruitsList(fruits)
+    }
+  }
+  useEffect(()=>(
+    setList()
+  ),[input])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Search Item</h1>
+        <input placeholder="Search fruits.." value={input} onChange={HandleChange} />
+        {
+            <ul>
+                {
+                fruitsList.map((fruit) =>(
+                    <li >{fruit}</li>
+                ))
+                }
+            </ul>
+        }
     </div>
   )
 }
+
 
 export default App
